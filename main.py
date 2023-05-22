040fd1f96efa5bcb4ce60a9e80f44ec91d4911ba
vojtakarasek <vojta.karasek1@mail.com>
2023-05-22 14:37:21 +0200

    mrizka

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-import tkinter
+from tkinter import *
+
+hlavni = Tk()
+
+w = Canvas(hlavni, width=506, height=506)
+w.pack()
+
+#creating table 10x10
+a = 5
+b = 5
+for x in range(11):
+    w.create_line(5,a,505,b)
+    a += 50
+    b += 50
+a = 5
+b = 5
+for x in range(11):
+    w.create_line(a,5,b,505)
+    a += 50
+    b += 50
 
 class Maze:
     def __init__(self):
         pass
+ 
+    def walls(self, canvas):
+        b = open("cisla.txt", "r")
 
-    def is_wall(x, y, direction): bool
 
-class Robot:
+    #def is_wall(x, y, direction): bool
 
 
-class Transformator:
+
+#class Robot:
+
+
+#class Transformator:
+
+mainloop()
