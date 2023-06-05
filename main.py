999032f44c740a578cf11022f8e71ee7d855b74a
vojtakarasek <vojta.karasek1@mail.com>
2023-06-05 14:36:32 +0200

    upravy

--- main.py
+++ main.py
@@ -29,31 +29,54 @@ class MazeData:
 
     def wall(self):
         walls = []
+        doors = []
         wall_x = 0
         wall_y = 0
+
         for _ in self.data[0]:
             for i in self.data[0][wall_x]:
                 if i == 1:
-                    walls.append((wall_x, wall_y))
-                wall_y += 1
+                    walls.append((wall_x, wall_y))  
+                elif i == 2:
+                    doors.append((wall_x, wall_y))
+                
+                if wall_y < len(self.data[0]):
+                    wall_y += 1
+                else:
+                    pass    
             wall_x += 1
 
         for _ in self.data[1]:
             for i in self.data[1][wall_y]:
                 if i == 1:
                     walls.append((wall_x, wall_y))
-                wall_x += 1
-            wall_y += 1
+                elif i == 2:
+                    doors.append((wall_x, wall_y))
+                
+                if wall_x < len(self.data[1]):
+                    wall_x += 1
+                else:
+                    pass
+            if wall_y < len(self.data[1]):
+                wall_y += 1
+            else:
+                pass  
+        print(walls, doors)
 
 
-    # def is_wall(self, row, column, direction): bool  #direction l,t,r,b
-        # first_array_idx = 0
-        # if (direction in ['t', 'b']):
-            # first_array_idx = 1
-        # return data[first_array_idx][]
 
-class Render:
-    def __init__(self):
+    #def is_wall(self, row, column, direction): bool  #direction l,t,r,b
+        #first_array_idx = 0
+        #if (direction in ['t', 'b']):
+            #first_array_idx = 1
+        #return data[first_array_idx][]
+
+#class Render:
+    #def __init__(self):
+        #pass
+
+    #def draw(self)
+        
 
 
 # class Robot:
@@ -61,5 +84,5 @@ class Render:
 
 # class Transformator:
 
-data = MazeData("cisla.txt")
+data = MazeData("cisla.txt").wall()
 mainloop()
